8f29afd958e9d0878445251788a49f18e09543fe
RianIhsan <rianwhatsup@gmail.com>
2023-04-14 02:03:00 +0700

    fix env railways

--- database/storage/postgres.go
+++ database/storage/postgres.go
@@ -14,10 +14,11 @@ type Config struct {
 	User     string
 	DBName   string
 	SSLMode  string
+	DBURL    string
 }
 
 func ConnectDB(config *Config) (*gorm.DB, error) {
-	dsn := fmt.Sprintf("host=%s port=%s password=%s user=%s dbname=%s sslmode=%s", config.Host, config.Port, config.Password, config.User, config.DBName, config.SSLMode)
+	dsn := fmt.Sprintf("host=%s port=%s password=%s user=%s dbname=%s sslmode=%s, dburl=%s", config.Host, config.Port, config.Password, config.User, config.DBName, config.SSLMode, config.DBURL)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
